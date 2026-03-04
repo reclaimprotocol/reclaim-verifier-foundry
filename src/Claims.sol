4655a65efa9f85ebd8dae5eee761c3d02ef6aaa1
hadi-saleh14 <hadi@creatoros.co>
2026-03-04 18:41:24 +0800

    remove dependency

--- src/Claims.sol
+++ src/Claims.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "./ECDSA.sol";
 import "./StringUtils.sol";
 
 /**
